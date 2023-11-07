b09692511f696aab461818bce5e0d3d68cb1c34b
mahi3crab <mahi3@hawaii.edu>
2023-11-06 23:50:38 -1000

    homepage added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,16 +8,28 @@ const Home = () => {
     return (
       <div className = "home-contents">
         <div className="army-bar">
-          <strong>{currentUser.first_name} {currentUser.last_name}'s</strong> Homepage
-        </div>
-        <div className ="side-nav-bar">
-          <a href="/home">Home</a>
-          <a href="/profile">Profile</a>
-          <a href="/home">Job Opportunities</a>
-          <a href="/home">Resumes</a>
-          <a href="/home">Sign-out</a>
+          <div>
+            <img src="/images/usar_logo1_rev_rgb_300ppi.png" width="200px" className="logo"></img>
+            <a href="/home" className="nav-bar">HOME</a>
+            <a href="/profile" className="nav-bar">PROFILE</a>
+            <a href="/home" className="nav-bar">JOB OPPORTUNITIES</a>
+            <a href="/home" className="nav-bar">RESUME</a>
+            <a href="/home" className="nav-bar">SIGN-OUT</a>
+          </div>
         </div>
 
+        <div className="header">
+          <div className='wrapl'>
+            <img src="/images/soldiers/230702-A-GS113-011.jpg" height="350px" margin-bottom="50px"></img>
+          </div>
+            <img src="/images/9thMSC-1.png" height="250px"></img>
+            <button className="critical-vac">CRITICAL VACANCIES</button>
+        </div>
+        <div className="main">
+          <p className="main-left">Kalena Market is a dynamic platform where individuals nuture and showcase their unique skills and talents. Join us on a journey where your skills and ambitions meet endless possibilities. Find your path, connect with like-minded individuals, and unlock the full potential of your talents at Kalena Market.</p>
+          <img src="/images/soldiers/_DSC1757.jpg" height="275px" className="main-left-carousel"></img>
+
+        </div>
       </div>
         
         

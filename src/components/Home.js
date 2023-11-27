5a383cc8c11e428fa5f6a7a35b2a1f9d85d1b072
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2023-11-26 20:07:03 -1000

    updated home and fixed website link bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import AuthService from '../services/auth.service';
-import "./Home.css"
+import React from "react";
+import AuthService from "../services/auth.service";
+import ImageCarousel from "./ImageCarousel";
+import "./Home.css";
 
 const Home = () => {
-    const currentUser = AuthService.getCurrentUser();
+  const currentUser = AuthService.getCurrentUser();
 
-    return (
-      <div className = "home-contents">
-        <div className="header">
-          <div className='wrapl'>
-            <img src="/images/soldiers/230702-A-GS113-011.jpg" height="350px" margin-bottom="50px"></img>
-          </div>
-            <img src="/images/9thMSC-1.png" height="250px"></img>
-            <button className="critical-vac">CRITICAL VACANCIES</button>
+  return (
+    <div className="home-contents">
+      <div className="header">
+        <div className="wrapl">
+          <ImageCarousel />
         </div>
-        <div className="main">
-          <p className="main-left">Kalena Market is a dynamic platform where individuals nuture and showcase their unique skills and talents. Join us on a journey where your skills and ambitions meet endless possibilities. Find your path, connect with like-minded individuals, and unlock the full potential of your talents at Kalena Market.</p>
-          <img src="/images/soldiers/_DSC1757.jpg" height="275px" className="main-left-carousel"></img>
-
-        </div>
+        <img src="/images/9thMSC-1.png" height="250px"></img>
+        <button className="critical-vac">CRITICAL VACANCIES</button>
       </div>
-        
-        
-    );
+
+      <div className="main">
+        <p className="main-left">
+          Kalena Market is a dynamic platform where individuals nuture and
+          showcase their unique skills and talents. Join us on a journey where
+          your skills and ambitions meet endless possibilities. Find your path,
+          connect with like-minded individuals, and unlock the full potential of
+          your talents at Kalena Market.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,90 +1,135 @@
-import React from 'react';
-import AuthService from '../services/auth.service';
-import {Link} from 'react-router-dom';
-import { Row, Col, Tab, Tabs, ListGroup} from 'react-bootstrap';
-import EditProfileModal from '../components/EditProfileModal.js'
-import ChangePassModal from '../components/ChangePassModal.js'
-import Favorites from '../components/Favorites.js'
+import React from "react";
+import AuthService from "../services/auth.service";
+import { Link } from "react-router-dom";
+import { Row, Col, Tab, Tabs, ListGroup } from "react-bootstrap";
+import EditProfileModal from "../components/EditProfileModal.js";
+import ChangePassModal from "../components/ChangePassModal.js";
+import Favorites from "../components/Favorites.js";
 
+const Profile = () => {
+  const currentUser = AuthService.getCurrentUser();
 
-const Profile= () => {
-    const currentUser = AuthService.getCurrentUser();
+  const name = currentUser.first_name + " " + currentUser.last_name;
+  const phone =
+    "(" +
+    currentUser.phone_number.slice(0, 3) +
+    ") " +
+    currentUser.phone_number.slice(3, 6) +
+    "-" +
+    currentUser.phone_number.slice(6, 10);
+  const email = currentUser.email;
+  let user_rank = currentUser.user_rank;
+  let location = currentUser.location;
+  let website = currentUser.link;
+  let image = currentUser.image;
+  console.log(currentUser.image);
+  if (user_rank == null) {
+    user_rank = "edit profile";
+  }
+  if (location == null) {
+    location = "edit profile";
+  }
+  if (image == null) {
+    image = "/images/userProfile.png";
+  }
+  if (website == null) {
+    website = "error";
+  }
 
-    const name = currentUser.first_name + " " +currentUser.last_name;
-    const phone = "(" + currentUser.phone_number.slice(0,3) + ") " + currentUser.phone_number.slice(3,6) + "-" + currentUser.phone_number.slice(6,10);
-    const email = currentUser.email;
-    let user_rank = currentUser.user_rank;
-    let location = currentUser.location;
-    let website = currentUser.link;
-    let image = currentUser.image;
-    console.log(currentUser.image);
-    if(user_rank == null) {
-        user_rank = "edit profile";
-    }
-    if(location == null) {
-        location = "edit profile";
-    }
-    if(image == null) {
-        image = "/images/userProfile.png";
-    }
-    if(website == null) {
-        website = "error";
-    }
-    
-    return (
-        <Row className='p-5 justify-content-center'>
+  return (
+    <Row className="p-5 justify-content-center">
+      <Col xs={6} md={4}>
+        <img
+          className="rounded-circle border border-2"
+          src={image}
+          alt="profile"
+          width="250px"
+        />
 
-            <Col xs={6} md={4}>
-                
-                <img className='rounded-circle border border-2' src={image} alt='profile' width="250px"/>
-                
-                <ListGroup.Item className='d-flex justify-content-end border border-0' style={{width: "250px"}}>
-                        <Link>Change Picture  <i className="bi bi-file-arrow-up-fill"></i></Link>
-                </ListGroup.Item>
-                <div className='my-4'>
-                    <p><strong>Name: </strong>{name}</p>
-                    <p><strong>Rank: </strong>{user_rank}</p>
-                    <p><strong>Location: </strong>{location}</p>
-                    <p><strong>Phone Number: </strong>{phone}</p>
-                    <p><strong>Email: </strong>{email}</p>
-                    <Link to={`/${website}`} target="_blank"><strong>Personal Website</strong></Link>
-                </div>
-                
-                
-                <ListGroup.Item className='d-flex justify-content-end border border-0' style={{width: "250px"}}><EditProfileModal /></ListGroup.Item>
-                <ListGroup.Item className='d-flex justify-content-end border border-0' style={{width: "250px"}}><ChangePassModal /></ListGroup.Item>
-                
+        <ListGroup.Item
+          className="d-flex justify-content-end border border-0"
+          style={{ width: "250px" }}
+        >
+          <Link>
+            Change Picture <i className="bi bi-file-arrow-up-fill"></i>
+          </Link>
+        </ListGroup.Item>
+        <div className="my-4">
+          <p>
+            <strong>Name: </strong>
+            {name}
+          </p>
+          <p>
+            <strong>Rank: </strong>
+            {user_rank}
+          </p>
+          <p>
+            <strong>Location: </strong>
+            {location}
+          </p>
+          <p>
+            <strong>Phone Number: </strong>
+            {phone}
+          </p>
+          <p>
+            <strong>Email: </strong>
+            {email}
+          </p>
+          {website ? (
+            <Link to={website} target="_blank">
+              <strong>Personal Website</strong>
+            </Link>
+          ) : (
+            <Link to={`/error`} target="_blank">
+              <strong>Personal Website</strong>
+            </Link>
+          )}
+        </div>
 
-                
+        <ListGroup.Item
+          className="d-flex justify-content-end border border-0"
+          style={{ width: "250px" }}
+        >
+          <EditProfileModal />
+        </ListGroup.Item>
+        <ListGroup.Item
+          className="d-flex justify-content-end border border-0"
+          style={{ width: "250px" }}
+        >
+          <ChangePassModal />
+        </ListGroup.Item>
+      </Col>
 
-            </Col>
-
-            <Col xs={12} md={8} className='fluid'>
-                <Tabs
-                    defaultActiveKey="favorites"
-                    id="justify-tab-example"
-                    justify
-                >
-                    <Tab className='border border-1 py-4 scrollbar overflow-auto w-100' style={{height: '650px'}} eventKey="favorites" title="Favorites">
-                        <Favorites/>
-                    </Tab>
-                    <Tab className='border border-1 py-4 scrollbar overflow-auto w-100' style={{height: '650px'}} eventKey="applied" title="Applied">
-                        Tab content for vacancies applied
-                    </Tab>
-                    <Tab className='border border-1 py-4 scrollbar overflow-auto w-100' style={{height: '650px'}} eventKey="resumes" title="Resumes">
-                        Tab content for resumes and related documents
-                    </Tab>
-                </Tabs>
-            </Col>
-
-
-
-
-        </Row>
-
-
-
-);
+      <Col xs={12} md={8} className="fluid">
+        <Tabs defaultActiveKey="favorites" id="justify-tab-example" justify>
+          <Tab
+            className="border border-1 py-4 scrollbar overflow-auto w-100"
+            style={{ height: "650px" }}
+            eventKey="favorites"
+            title="Favorites"
+          >
+            <Favorites />
+          </Tab>
+          <Tab
+            className="border border-1 py-4 scrollbar overflow-auto w-100"
+            style={{ height: "650px" }}
+            eventKey="applied"
+            title="Applied"
+          >
+            Tab content for vacancies applied
+          </Tab>
+          <Tab
+            className="border border-1 py-4 scrollbar overflow-auto w-100"
+            style={{ height: "650px" }}
+            eventKey="resumes"
+            title="Resumes"
+          >
+            Tab content for resumes and related documents
+          </Tab>
+        </Tabs>
+      </Col>
+    </Row>
+  );
 };
 
 export default Profile;

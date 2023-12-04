99fad522f8d18dc7fad55fc4e60688b98b4ca7f9
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2023-12-04 00:32:49 -1000

    update cards

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,65 +1,64 @@
-import React, {useState, useEffect} from 'react';
-import UserService from '../services/user.service';
-import AuthService from '../services/auth.service';
-import {Link} from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import JobsModal from '../components/JobsModal.js';
-import Hearts from '../components/Hearts.js';
+import React, { useState, useEffect } from "react";
+import UserService from "../services/user.service";
+import AuthService from "../services/auth.service";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import JobsModal from "../components/JobsModal.js";
+import Hearts from "../components/Hearts.js";
 
 const filterArray = (jobs, faves) => {
-    var filtered = [];
-    for(var j in jobs){
-	for(var f in faves) {
-	   if(jobs[j].id === faves[f].job_id){
-		filtered.push(jobs[j]);
-	   }
-	}
-    };
-    
-    return filtered
+  var filtered = [];
+  for (var j in jobs) {
+    for (var f in faves) {
+      if (jobs[j].id === faves[f].job_id) {
+        filtered.push(jobs[j]);
+      }
+    }
+  }
+
+  return filtered;
 };
 
 const Favorites = () => {
-    const currentUser = AuthService.getCurrentUser();
-    
-    const [jobs, setJobs] = useState([]);
-    //const [favorites, setFavorites] = useState([]);
-    const [faveIDs, setFaveIDs] = useState([]);
-    //const [loading, setLoading] = useState(true);
-    //const [error, setError] = useState(null);
-    //const [likedJobs, setLikedJobs] = useState(false);
+  const currentUser = AuthService.getCurrentUser();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try{
-                AuthService.getLikes(currentUser.id).then(res => {
-                console.log(res.data);
-    	    	setFaveIDs(res.data);
-                })
-            
-            }catch (err) {
-            	console.log(err);
-            }
+  const [jobs, setJobs] = useState([]);
+  //const [favorites, setFavorites] = useState([]);
+  const [faveIDs, setFaveIDs] = useState([]);
+  //const [loading, setLoading] = useState(true);
+  //const [error, setError] = useState(null);
+  //const [likedJobs, setLikedJobs] = useState(false);
 
-            try {
-                const response = await UserService.getJobs();
-                const jobData = response.data;
-                console.log(jobData);
-                setJobs(jobData);
-                //setLoading(false);
-            }catch (err) {
-                console.error("Error fetchng job data:", err);
-                //setError(err);
-                //setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        AuthService.getLikes(currentUser.id).then((res) => {
+          console.log(res.data);
+          setFaveIDs(res.data);
+        });
+      } catch (err) {
+        console.log(err);
+      }
 
-           fetchData().then();
-    }, []);
+      try {
+        const response = await UserService.getJobs();
+        const jobData = response.data;
+        console.log(jobData);
+        setJobs(jobData);
+        //setLoading(false);
+      } catch (err) {
+        console.error("Error fetchng job data:", err);
+        //setError(err);
+        //setLoading(false);
+      }
+    };
 
-    /*const toggleLike = (jobId) => {
+    fetchData().then();
+  }, []);
+
+  /*const toggleLike = (jobId) => {
         setLikedJobs((prevLikedJobs) =>
             ({...prevLikedJobs,
             [jobId]: !prevLikedJobs[jobId],
@@ -73,79 +72,94 @@ const Favorites = () => {
         return <div> Error: {error.message}</div>
     }
     */
-    //These are the users favorites
-    const filtered = filterArray(jobs, faveIDs);
-    const empty = filtered.length === 0;
+  //These are the users favorites
+  const filtered = filterArray(jobs, faveIDs);
+  const empty = filtered.length === 0;
 
-    return (
-        <div className='d-flex justify-content-center'>
-            {empty? (
-                <Container className="py-5 w-100">
-                    <p>You have no favorites, go to the <Link to="/jobs">jobs</Link> page to add to favorites.</p>
-                    <img className='opacity-50 border border-sceondary border-5 rounded-4' src="/images/emptyFaves.gif" alt="Intructions to like a job" width="500px"/> 
-                </Container>
-            ):(
-                    <div className='p-3'>
-                    <div className="job-cards">
-                    {filtered.map((job) => (
-
-                        <Card bg="light" key="Light" text="dark" className='mb-4 w-100'>
-                            <Card.Header className='border-bottom-0 pb-0 bg-light'>
-                                <h3>{job.title}</h3>
-                            </Card.Header>
-                            <Card.Body
-                                className='border-top-0 border-bottom-0 py-0 scrollable overflow-hidden'
-                                style={{height: '125px'}}>
-                              <Row key={job.id}>
-                                <p>{job.description}</p>
-                              </Row>
-                              <Row>
-                              <div className='d-flex justify-content-center'>
-                              <Row className='w-50'>
-                                <p><strong>Open & Closing Dates: </strong>{job.open} - {job.close}</p>
-                                <p><strong>Pay Scale & Grade: </strong>{job.pay}</p>
-                              </Row>
-                              <Row className='w-50'>
-                                <p><strong>Location: </strong>{job.location}</p>
-                                <p><strong>Schedule: </strong>{job.schedule}</p>
-                              </Row>
-                              </div>
-                            </Row>
-                            </Card.Body>
-                            <Card.Footer className='d-flex justify-content-between border-top-0 py-0 bg-light'>
-                                <JobsModal 
-                            	id={job.id} 
-                            	title={job.title} 
-                            	description={job.description} 
-                            	open={job.open}
-                            	close={job.close}
-                            	pay={job.pay} 
-                            	location={job.location} 
-                            	remote={job.remote} 
-                            	travel={job.travel} 
-                            	schedule={job.schedule} 
-                            	duties={job.duties}
-                            	conditions={job.conditions}
-                            	qualifications={job.qualifications}
-                            	education={job.education}
-                            	additional={job.additional}
-                            	/> 
-                                <Hearts user_id = {currentUser.id} job_id = {job.id}/>
-                            </Card.Footer>
-                        </Card>
-                        
-                            
-                    ))}
-                        
+  return (
+    <div className="d-flex justify-content-center">
+      {empty ? (
+        <Container className="py-5 w-100">
+          <p>
+            You have no favorites, go to the <Link to="/jobs">jobs</Link> page
+            to add to favorites.
+          </p>
+          <img
+            className="opacity-50 border border-sceondary border-5 rounded-4"
+            src="/images/emptyFaves.gif"
+            alt="Intructions to like a job"
+            width="500px"
+          />
+        </Container>
+      ) : (
+        <div className="p-3">
+          <div className="job-cards">
+            {filtered.map((job) => (
+              <Card bg="light" key="Light" text="dark" className="mb-4 w-100">
+                <Card.Header className="border-bottom-0 pb-0 bg-light">
+                  <h3>{job.title}</h3>
+                </Card.Header>
+                <Card.Body className="border-top-0 border-bottom-0 py-0">
+                  <Row key={job.id}>
+                    <p>{job.description}</p>
+                  </Row>
+                  <Row>
+                    <div className="d-flex justify-content-center">
+                      <Row className="w-50">
+                        <p>
+                          <strong>Open & Closing Dates: </strong>
+                          {job.open} - {job.close}
+                        </p>
+                        <p>
+                          <strong>Pay Scale & Grade: </strong>
+                          {job.pay}
+                        </p>
+                      </Row>
+                      <Row className="w-50">
+                        <p>
+                          <strong>Location: </strong>
+                          {job.location}
+                        </p>
+                        <p>
+                          <strong>Schedule: </strong>
+                          {job.schedule}
+                        </p>
+                      </Row>
                     </div>
-                        <div className='text-end'>
-                            <Button href="/profile" variant="secondary">Save</Button>
-                        </div>
-                </div>
-            )}
+                  </Row>
+                </Card.Body>
+                <Card.Footer className="d-flex justify-content-between border-top-0 py-0 bg-light">
+                  <JobsModal
+                    id={job.id}
+                    title={job.title}
+                    description={job.description}
+                    open={job.open}
+                    close={job.close}
+                    pay={job.pay}
+                    location={job.location}
+                    remote={job.remote}
+                    travel={job.travel}
+                    schedule={job.schedule}
+                    duties={job.duties}
+                    conditions={job.conditions}
+                    qualifications={job.qualifications}
+                    education={job.education}
+                    additional={job.additional}
+                  />
+                  <Hearts user_id={currentUser.id} job_id={job.id} />
+                </Card.Footer>
+              </Card>
+            ))}
+          </div>
+          <div className="text-end">
+            <Button href="/profile" variant="secondary">
+              Save
+            </Button>
+          </div>
         </div>
-
-      );
-}
+      )}
+    </div>
+  );
+};
 
 export default Favorites;
